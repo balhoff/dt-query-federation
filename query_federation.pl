0d0ee2f46096da8ab4301867f040f1cad3be9aa6
Chris Mungall <cjm@berkeleybop.org>
2017-10-29 20:38:08 -0700

    genericizing

--- query_federation.pl
+++ query_federation.pl
@@ -15,26 +15,12 @@ has_download_uri(Triples, Other) :- member(rdf(_, 'http://www.w3.org/ns/hydra/co
 has_triples(URI, Triples) :- downloaded_triples(URI, Triples).
 has_triples(URI, MoreTriples) :- downloaded_triples(URI, Triples), has_download_uri(Triples, Other), has_triples(Other, MoreTriples).
 
-% FIXME the query parameters used for the call should be figured out from triples in the service endpoint
-tpfuri_SPO(S, P, O, URI) :- atom(S), atom(P), atom(O), uri_query_components(QueryString, [subject=S, predicate=P, object=O]), tpf_endpoint(Endpoint), atomic_concat(Endpoint, QueryString, URI).
-tpfuri_SP(S, P, URI) :- atom(S), atom(P), uri_query_components(QueryString, [subject=S, predicate=P]), tpf_endpoint(Endpoint), atomic_concat(Endpoint, QueryString, URI).
-tpfuri_SO(S, O, URI) :- atom(S), atom(O), uri_query_components(QueryString, [subject=S, object=O]), tpf_endpoint(Endpoint), atomic_concat(Endpoint, QueryString, URI).
-tpfuri_PO(P, O, URI) :- atom(P), atom(O), uri_query_components(QueryString, [predicate=P, object=O]), tpf_endpoint(Endpoint), atomic_concat(Endpoint, QueryString, URI).
-tpfuri_S(S, URI) :- atom(S), uri_query_components(QueryString, [subject=S]), tpf_endpoint(Endpoint), atomic_concat(Endpoint, QueryString, URI).
-tpfuri_P(P, URI) :- atom(P), uri_query_components(QueryString, [predicate=P]), tpf_endpoint(Endpoint), atomic_concat(Endpoint, QueryString, URI).
-tpfuri_O(O, URI) :- atom(O), uri_query_components(QueryString, [object=O]), tpf_endpoint(Endpoint), atomic_concat(Endpoint, QueryString, URI).
-tpfuri(URI) :- tpf_endpoint(URI).
-
-:- discontiguous rdf/3.
-rdf(S, P, O) :- atom(S), atom(P), atom(O), tpfuri_SPO(S, P, O, URI), has_triples(URI, T), member(rdf(S, P, O), T).
-rdf(S, P, O) :- atom(S), atom(P), var(O), tpfuri_SP(S, P, URI), has_triples(URI, T), member(rdf(S, P, O), T).
-rdf(S, P, O) :- atom(S), var(P), atom(O), tpfuri_SO(S, O, URI), has_triples(URI, T), member(rdf(S, P, O), T).
-rdf(S, P, O) :- var(S), atom(P), atom(O), tpfuri_PO(P, O, URI), has_triples(URI, T), member(rdf(S, P, O), T).
-rdf(S, P, O) :- atom(S), var(P), var(O), tpfuri_S(S, URI), has_triples(URI, T), member(rdf(S, P, O), T).
-rdf(S, P, O) :- var(S), atom(P), var(O), tpfuri_P(P, URI), has_triples(URI, T), member(rdf(S, P, O), T).
-rdf(S, P, O) :- var(S), var(P), atom(O), tpfuri_O(O, URI), has_triples(URI, T), member(rdf(S, P, O), T).
-rdf(S, P, O) :- var(S), var(P), var(O), tpfuri(URI), has_triples(URI, T), member(rdf(S, P, O), T).
-
+rdf(S, P, O) :-
+        findall(A=V,(member(A=V,[subject=S,predicate=P,object=O]),atom(V)),AVs),
+        uri_query_components(QueryString, AVs),
+        tpf_endpoint(Endpoint),
+        atomic_concat(Endpoint, QueryString, URI),
+        has_triples(URI, T), member(rdf(S, P, O), T).
 
 % Examples
 
